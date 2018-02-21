1aadf929432c37bc9db85b3da92f96e6a97de79f
Ahreum Kim <kim.ah.rm@gmail.com>
2018-02-21 19:35:49 +0900

    Add (real~). 02 simple realtime search

--- 02_simpleRealtimeSearch/src/App.js
+++ 02_simpleRealtimeSearch/src/App.js
@@ -1,21 +1,171 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+/**
+ * SearchApp (멍청)
+ *  - Header (멍청)
+ *  - MainSection (똑똑)
+ *    - SearchInput (멍청? 똑똑?)
+ *    - SearchList (똑똑?)
+ *      - SearchListItem (멍청)
+ *      - SearchListItem (멍청)
+ *      - ...
+ */
+
+
+const SearchListItem = ({ artistName }) => (
+  <li>{artistName}</li>
+);
+
+
+class SearchList extends Component {
+  constructor() {
+    super();
+    this.totalList = [];
+    this.state = {
+      filteredList: [],
+      isLoading: true,
+      isFailed: false
+    };
+  }
+  componentDidMount() {
+    const reqUrlForArtist = 'https://gist.githubusercontent.com/LumiLoves/6c87a4cf79d5ce2410e849abc8e3f81f/raw/d162071c9db32c35c69e7a2656f1e78833329b85/list.json';
+
+    setTimeout(() => {
+      this.getJSON(reqUrlForArtist, null, (json) => {
+        this.totalList = json.Reggae;
+        this.setState({
+          isLoading: false,
+          filteredList: this.totalList
+        });
+      });
+    }, 1500);
+  }
+
+  getJSON(url, data, success) {
+    const xhr = new XMLHttpRequest();
+    let resData = null;
+
+    xhr.open('GET', url);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== 4) { return false; }
+      if (xhr.status !== 200) { this.setState({ isFailed: true }); }
+
+      resData = JSON.parse(xhr.responseText);
+      (typeof success === 'function') && success(resData);
+    };
+    xhr.send(data);
+  }
+  filter(key) {
+    const totalList = this.totalList;
+    const regexKey = new RegExp(key, 'i');
+    let resultArr = [];
+
+    if (key === '') { return totalList; }
+    totalList.forEach((elem) => { elem.match(regexKey) && resultArr.push(elem); });
+    return resultArr;
+  }
+  makeHtml(listArr) {
+    if (!listArr || !listArr.length) { return ''; }
+
+    let result = '';
+    listArr.forEach((item) => { result += `<li>${item}</li>`; });
+    return result;
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="search-list">
+          <p className="progress-notice">Loading ... (일부러 딜레이 줌)</p>
+        </div>
+      );
+    }
+
+    if (this.state.isFailed) {
+      return (
+        <div className="search-list">
+          <p className="progress-notice">Fail</p>
+        </div>
+      );
+    }
+
+    const filteredList = this.filter(this.props.searchkey);
+    const searchListItems = filteredList.map((artistName, index) => (
+      <SearchListItem artistName={artistName} key={index} />
+    ));
+
+    if (filteredList.length === 0) {
+      return (
+        <div className="search-list">
+          <p className="progress-notice">No Results :0</p>
+        </div>
+      );
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="search-list">
+        <ul>{searchListItems}</ul>
       </div>
     );
   }
 }
 
-export default App;
+
+class SearchInput extends Component {
+  render() {
+    return (
+      <div className="search-input">
+        <h2>Search: </h2>
+        <input type="text" placeholder="Search Keyword ..."
+          ref={ref => this.$input = ref}
+          onChange={this.props.handleChangeInput}
+        />
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.$input.focus();
+  }
+}
+
+
+class MainSection extends Component {
+  constructor() {
+    super();
+    this.state = { searchKey: '' };
+  }
+  handleChangeInput({ target }) {
+    const inputVal = target.value;
+    this.setState({ searchKey: inputVal });
+  }
+  render() {
+    return (
+      <main id="main">
+        <SearchInput handleChangeInput={this.handleChangeInput.bind(this)} />
+        <SearchList searchkey={this.state.searchKey} />
+      </main>
+    );
+  }
+}
+
+
+const Header = () => (
+  <header id="header">
+    <h1>React Search!</h1>
+    <p className="desc">
+      Here is a list of Reggae artists rendered from a JSON object
+    </p>
+  </header>
+);
+
+
+const SearchApp = () => (
+  <div className="search-app">
+    <Header />
+    <MainSection />
+  </div>
+);
+
+
+export default SearchApp;
--- README.md
+++ README.md
@@ -4,17 +4,17 @@
 ## 스터디 방식
 * 같은 주제의 프로젝트를 개발한 후 코드 리뷰 (1 hour)
   * 1~2명이 오프라인 코드 리뷰
-  * 나머지는 온라인 코드 리뷰 
+  * 나머지는 온라인 코드 리뷰
 * 어려운 내용에 대해서 같이 논의하기 (1 hour)
 
 ## 회차별 내용
 [예제선정참고](http://sean-smith.me/assets/portfolio/25-react-projects/index.html)
 
-1. [Markdown live previewer 만들기 및 리뷰](https://github.com/kimally/React-Study/tree/master/01_markdownPreviewer)
-2. Simple realtime search 만들기 및 리뷰
-3. 
-4. 
-5. 
-6. 
-7. 
-8. 
+1. [Markdown live previewer 만들기 및 리뷰](https://github.com/lumiloves/Study_React/tree/master/01_markdownPreviewer)
+2. [Simple realtime search 만들기 및 리뷰](https://github.com/lumiloves/Study_React/tree/master/02_simpleRealtimeSearch)
+3.
+4.
+5.
+6.
+7.
+8.
